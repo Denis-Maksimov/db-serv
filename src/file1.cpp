2ced39ffa026824312195670f3918df7a8520f4c
Maksimov <dn.maksimow@gmail.com>
2019-12-21 13:45:29 +0300

    Добавлено уведомление о лицензии GNU GPL

--- src/file1.cpp
+++ src/file1.cpp
@@ -1,3 +1,30 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+////--------------------- Уведомление о лицензии GPL --------------------------------/////
+//////////////////////////////////////////////////////////////////////////////////////////
+/* Drying monitor
+ * Copyright (C) 2019 Maksimov Denis dn.maksimow@gmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+//////////////////////////////////////////////////////////////////////////////////////////
+// Иными словами, 
+// 1) при распространении программы, обязательно прикреплять к ней исходники
+// или уведомление о том как их получить (БЕСПЛАТНО!!!)
+// 2) GNU GPL — копилефтная лицензия, и требует, чтобы исходные коды производных работ были открытыми под ней же.
+// 3) Обязательно в производные работы прикреплять уведомление выше
+// Несоблюдение пунктов лицензии GNU GPL нарушает авторские права Free Software Foundation
+//////////////////////////////////////////////////////////////////////////////////////////
 #include "file1.hpp"
 #define f1 setup_from_files
 
