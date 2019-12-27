e5f962f0270d760d0a5d5a45e4d6e349f9f07c7a
Maksimov <dn.maksimow@gmail.com>
2019-12-27 19:46:45 +0300

    Настроил GTK, сделал цикличный вызов БД

---
+++ src/Querry/main_Querry.cpp
@@ -0,0 +1,16 @@
+#include "main_Querry.hpp"
+
+
+int main_Querry(int argc, const char** argv){
+   // #define INI_FILE_WHITH_NAMES_OF_DATABASES argv[0]
+   // #define INI_FILE_WHITH_NAMES_OF_QUERRYS argv[1]
+
+    puts("db");
+  //  char* get_line(int line_number,char* buffer,std::size_t size_of_bufer);
+    //-----------------------------------------------
+       SQLquery obj("./querys.data");   
+       obj.execute_querry("./TEST1.db", Query::new_table);
+    //-----------------------------------------------
+    return 0;
+
+}
---
+++ src/Querry/main_Querry.hpp
@@ -0,0 +1,4 @@
+#include "file1.hpp"
+#include "querys.hpp"
+
+int main_Querry(int argc, const char** argv);
